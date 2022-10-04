49cd66f45120d2107d3eeb1666887537fb19194e
DavidJennicson <85760802+DavidJennicson@users.noreply.github.com>
2022-10-05 01:44:52 +0530

    login form added

--- hackover/src/App.js
+++ hackover/src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import Card from './components/Card';
-import Logform from './components/Logform';
+
 // import Table from './components/Table';
 import Navbar from './components/Navbar';
 import RevGraph from './components/RevGraph';
 import Table from './components/Table';
 import React from 'react';
+import Login from './components/Login';
 
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
 <Card/>
 <br />
 <Table/> */}
-<Logform/>
+<Login/>
    </>
   );
 }
